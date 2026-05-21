847907bf2502c831f8b9f6826c3c0f777a830fb3
sk8thing <sk8thing@tuta.com>
2026-05-21 16:14:07 +0300

    feat(neovim): configure vimtex synctex and latexmk options

--- .config/nvim/lua/plugins/vimtex.lua
+++ .config/nvim/lua/plugins/vimtex.lua
@@ -4,6 +4,14 @@ return {
     lazy = false,
     init = function()
         vim.g.vimtex_view_method = "zathura"
+        vim.g.vimtex_view_general_options = "--synctex-forward @line:@col:@tex @pdf"
         vim.g.vimtex_compiler_method = "latexmk"
+        vim.g.vimtex_compiler_latexmk = {
+            options = {
+                "-xelatex",
+                "-interaction=nonstopmode",
+                "-synctex=1",
+            },
+        }
     end,
 }
